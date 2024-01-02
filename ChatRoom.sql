d696b8dc3caf3f4a847355a880cd4d93880634ed
Abhishek Pratap Singh <120483204+avsk3011@users.noreply.github.com>
2024-01-02 16:28:38 +0530

    Add files via upload

---
+++ ChatRoom.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 29, 2023 at 12:23 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ChatRoom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `msg`
+--
+
+CREATE TABLE `msg` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `msg` varchar(400) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `uname` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `uname`, `phone`) VALUES
+(1, '', ''),
+(2, 'abhishek ', '8756163538'),
+(3, 'abhishek ', '875616353826'),
+(4, 'abhishek ', '1234567891'),
+(5, 'abhishek ', '8888888888'),
+(6, 'aman', '222222222222'),
+(7, 'aman', '12312312312'),
+(8, '', '1111111111'),
+(9, 'abhishek ', '123123123123'),
+(10, 'aman', '789456123789'),
+(11, 'aman', '9999999999'),
+(12, 'aman', '999999999999'),
+(13, 'anaya', '9936453714'),
+(14, 'mohan', '123123123125'),
+(15, 'sachin', '7777777777'),
+(16, 'sonu', '6666666666'),
+(17, 'Anaya', '10000000000'),
+(18, 'Rahul', '20000000000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `msg`
+--
+ALTER TABLE `msg`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `msg`
+--
+ALTER TABLE `msg`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+
+$db=mysqli_connect("sql213.infinityfree.com","if0_35708570","EHYqMDQbtYzXAU","f0_35708570_chatapp");
+
+?>
